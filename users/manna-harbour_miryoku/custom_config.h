b137d0a2c581fa61c7a99569af5fbb9a35562a43
Javier Alba <fco.j.alba@gmail.com>
2023-01-15 17:40:55 -0600

    Decreased delay in tapping term, added missing columns to manipulate with single hand.

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -5,3 +5,24 @@
 
 #pragma once
 
+#define XXX KC_NO
+
+#define MIRYOKU_MAPPING( \
+      K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09, \
+      K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19, \
+      K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29, \
+      N30,  N31,  K32,  K33,  K34,         K35,  K36,  K37,  N38,  N39 \
+) \
+LAYOUT_split_3x6_3( \
+LT(U_FUN,KC_DEL),  K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09,  LT(U_MEDIA,KC_ESC), \
+LT(U_NUM,KC_BSPC),  K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19,  LT(U_NAV,KC_SPC), \
+LT(U_SYM,KC_ENT),  K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29,  LT(U_MOUSE,KC_TAB) , \
+                  K32,  K33,  K34,         K35,  K36,  K37 \
+)
+
+//setting qwerty by default
+#define MIRYOKU_LAYER_BASE \
+KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,              \
+LGUI_T(KC_A),      LALT_T(KC_S),      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      LGUI_T(KC_QUOT),   \
+LT(U_BUTTON,KC_Z), ALGR_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),    LT(U_BUTTON,KC_SLSH),\
+U_NP,              U_NP,              LT(U_MEDIA,KC_ESC),LT(U_NAV,KC_SPC),  LT(U_MOUSE,KC_TAB),LT(U_SYM,KC_ENT),  LT(U_NUM,KC_BSPC), LT(U_FUN,KC_DEL),  U_NP,              U_NP
